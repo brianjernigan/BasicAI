a8643f05d27fbe47abaafc143e559cf483ad44e4
brianjernigan <brobertjernigan@gmail.com>
2024-04-20 10:06:27 -0400

    Refactoring
    Split Player classes, working on enemy

---
+++ Assets/Scripts/Player/PlayerCollisions.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _healthText;
+    [SerializeField] private GameObject _lossPanel;
+    [SerializeField] private GameObject _gamePanel;
+    
+    private const float DamageInterval = 2f;
+    private float _damageTimer;
+    
+    private int Health { get; set; } = 4;
+    
+    private void ResetDamageTimer()
+    {
+        _damageTimer = DamageInterval;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Enemy")) return;
+        TakeDamage();
+        ResetDamageTimer();
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Enemy")) return;
+        if (_damageTimer > 0)
+        {
+            _damageTimer -= Time.deltaTime;
+        }
+        else
+        {
+            TakeDamage();
+            ResetDamageTimer();
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Enemy")) return;
+        _damageTimer = 0;
+    }
+
+    private void TakeDamage()
+    {
+        Health = Mathf.Max(--Health, 0);
+        UpdateHealthText(Health);
+        
+        if (Health > 0) return;
+        
+        _lossPanel.SetActive(true);
+        _gamePanel.SetActive(false);
+        Time.timeScale = 0;
+    }
+    
+    private void UpdateHealthText(int health)
+    {
+        _healthText.text = $"Health: {health}";
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -20,27 +20,15 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
     [SerializeField] private Rigidbody _rb;
-    [SerializeField] private TMP_Text _healthText;
-    [SerializeField] private GameObject _lossPanel;
-    [SerializeField] private GameObject _gamePanel;
 
     private const float MoveSpeed = 2.5f;
     private const float RotateSpeed = 10f;
-
-    private const float DamageInterval = 2f;
-    private float _damageTimer;
     
     private bool IsWalking { get; set; }
-    private int Health { get; set; } = 4;
 
     private void Awake()
     {
         Time.timeScale = 1f;
-    }
-    
-    private void ResetDamageTimer()
-    {
-        _damageTimer = DamageInterval;
     }
     
     private void FixedUpdate()
@@ -68,48 +56,6 @@ public class PlayerController : MonoBehaviour
         }
 
         _anim.SetBool("IsWalking", IsWalking);
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (!other.gameObject.CompareTag("Enemy")) return;
-        TakeDamage();
-        ResetDamageTimer();
-    }
-
-    private void OnCollisionStay(Collision other)
-    {
-        if (!other.gameObject.CompareTag("Enemy")) return;
-        if (_damageTimer > 0)
-        {
-            _damageTimer -= Time.deltaTime;
-        }
-        else
-        {
-            TakeDamage();
-            ResetDamageTimer();
-        }
-    }
-
-    private void OnCollisionExit(Collision other)
-    {
-        if (!other.gameObject.CompareTag("Enemy")) return;
-        _damageTimer = 0;
-    }
-
-    private void TakeDamage()
-    {
-        Health = Mathf.Max(--Health, 0);
-        UpdateHealthText(Health);
-        if (Health > 0) return;
-        _lossPanel.SetActive(true);
-        _gamePanel.SetActive(false);
-        Time.timeScale = 0;
-    }
-
-    private void UpdateHealthText(int health)
-    {
-        _healthText.text = $"Health: {health}";
     }
 
     public void OnClickRestartButton()
